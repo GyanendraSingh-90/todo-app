e9bca51b5d64231278ed865cc8f53c5629426729
GyanendraSingh-90 <patel90shiva90@gmail.com>
2026-01-12 15:23:22 +0530

    Implemented edit, delete and complete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
 
 function App(){
+    const[todos, setTodos]= useState([]);
+
+    // Add ToDo
+    const addTodo =(text) =>{
+        setTodos([...todos, 
+            {id: Date.now(), text, completed: false}
+        ]);
+    }
+    
+
+    // Delete Todo
+    const deleteTodo =(id) =>{
+        setTodos(todos.filter(todo => todo.id !==id));
+    }
+
+    // Toggle Complete
+    const toggleTodo =(id) =>{
+        setTodos(
+            todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+        )
+    }
+
+
+    //Edit Todo
+    const editTodo =(id, newText) =>{
+        setTodos(
+            todos.map(todo => todo.id === id ? {...todo, text: newText} : todo)
+        )
+    }
     return(
-        <div>
-            <Header/>
+        <div className="app">
+            <Header addTodo={addTodo} />
+            <ToDoList 
+            todos={todos}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+            editTodo={editTodo}
+            />            
         </div>
     )
 }
-export default App
+export default App;
