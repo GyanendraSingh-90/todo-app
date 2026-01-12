72f1517d572d6b30d5e8a5ddee71585f025f0e24
GyanendraSingh-90 <patel90shiva90@gmail.com>
2026-01-12 16:02:07 +0530

    Importing index.css file to App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
-
+import "./index.css";
 function App(){
     const[todos, setTodos]= useState([]);
 
