e4d4491c30ec00de5007b3d79f7a15ae9f88274d
GyanendraSingh-90 <patel90shiva90@gmail.com>
2026-01-12 15:13:54 +0530

    some changes in ToDoItem.jsx

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function ToDoItem({todo, deleteTodo, toggleTodo, editTodo}){
+    const[isEditing, setIsEditing] = useState(false);
+    const[editText, setEditText] = useState(todo.text);
+
+    const handleEdit =() =>{
+        editTodo(todo.id,editText);
+        setIsEditing(false);
+    }
+
+    
+    return(
+        <div className="todo-items">
+            <input type="checkbox" checked={ToDoItems.completed} onChange={()=> ToggleTodo(todo.id)} />
+            { isEditing ? (
+                <input value ={editText} onChange={(e) => setEditText(e.target.value)} />
+            ) :(
+                <span className={todo.completed ? "completed" : ""}>
+                    {todo.text}
+                </span>
+            )}
+
+             { isEditing ? (
+                <button on onClick={handleEdit}>Save</button>
+            ) :(
+                <button onClick={() => setIsEditing(true)}>Edit</button>
+            )}
+
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </div>
+    );
+}
+
+export default ToDoItem;
